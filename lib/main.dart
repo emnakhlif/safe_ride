e6b880d974b37b216cc92ec41976d845a756b6ee
farah-17 <146463435+farah-17@users.noreply.github.com>
2025-04-22 16:49:29 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'screens/home_screen.dart'; // Import home screen
+import 'package:firebase_auth/firebase_auth.dart';
+import 'screens/home_screen.dart';
+import 'screens/create_account_screen.dart'; // Import Create Account screen
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp();  // Ensure Firebase is initialized
   runApp(const MyApp());
 }
 
@@ -20,8 +22,20 @@ class MyApp extends StatelessWidget {
         primaryColor: const Color(0xFF2D4356),
         scaffoldBackgroundColor: Colors.white,
       ),
-      home: const HomeScreen(),
+      home: _getInitialScreen(),
     );
+  }
+
+  // Function to check if the user is logged in and return the appropriate screen
+  Widget _getInitialScreen() {
+    // Get the current user from Firebase Authentication
+    User? user = FirebaseAuth.instance.currentUser;
+    
+    if (user != null) {
+      return const HomeScreen(); // If the user is logged in, show the HomeScreen
+    } else {
+      return const CreateAccountScreen(); // Otherwise, show the CreateAccountScreen
+    }
   }
 }
 
